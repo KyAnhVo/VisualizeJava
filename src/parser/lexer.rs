6fc70326db8cf6a452277ca23d1d377ad63e46cf
Ky Anh Vo <tommyvo0406@gmail.com>
2026-06-08 13:32:52 -0500

    lexer

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,30 +1,28 @@
-use crate::parser::token::{
-    Keyword::*,
-    Token::{self, *},
-};
-use std::{fs, io};
-struct Lexer {
-    str: Vec<char>,
+use crate::parser::token::Token::{self, *};
+use std::io;
+struct Lexer<'a> {
+    s: &'a str,
     ind: usize,
     curr_char: Option<char>,
 }
 
-impl Lexer {
-    pub fn new(file: String) -> io::Result<Self> {
-        Ok(Self {
-            str: fs::read_to_string(file)?.chars().collect(),
+impl<'a> Lexer<'a> {
+    pub fn new(s: &'a str) -> Self {
+        Self {
+            s,
             ind: 0,
             curr_char: Some('\0'),
-        })
+        }
     }
 
-    pub fn get_next_token(&mut self) -> Option<Token> {
+    pub fn get_next_token(&mut self) -> Option<Token<'a>> {
         loop {
             let next_char = self.get_next_char();
             if next_char.is_none() {
                 return Some(EOF);
             }
-            match *self.get_next_char()? {
+            match next_char? {
+                c if c.is_whitespace() => continue,
                 '{' => return Some(LBrace),
                 '}' => return Some(RBrace),
                 '[' => return Some(LBracket),
@@ -35,8 +33,9 @@ impl Lexer {
                 '@' => return Some(Annotation),
                 ',' => return Some(Comma),
                 '~' => return Some(Op("~")),
+                ';' => return Some(Semicolon),
                 '.' => {
-                    if self.peek_next_2() == (Some(&'.'), Some(&'.')) {
+                    if self.peek_next_2() == (Some('.'), Some('.')) {
                         self.get_next_char();
                         self.get_next_char();
                         return Some(Op("..."));
@@ -44,33 +43,345 @@ impl Lexer {
                         return Some(Dot);
                     }
                 }
+                c @ ('^' | '%' | '!') => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(match c {
+                            '^' => Assignment("^="),
+                            '%' => Assignment("%="),
+                            '!' => Op("!="),
+                            _ => unreachable!(),
+                        });
+                    }
+                    _ => {
+                        return Some(Op(match c {
+                            '^' => "^",
+                            '%' => "%",
+                            '!' => "!",
+                            _ => unreachable!(),
+                        }));
+                    }
+                },
+                ':' => match self.peek_next_char() {
+                    Some(':') => {
+                        self.get_next_char();
+                        return Some(Op("::"));
+                    }
+                    _ => return Some(Op(":")),
+                },
+                '=' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Op("=="));
+                    }
+                    _ => return Some(Assignment("=")),
+                },
+                '+' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("+="));
+                    }
+                    Some('+') => {
+                        self.get_next_char();
+                        return Some(Op("++"));
+                    }
+                    _ => return Some(Op("+")),
+                },
+                '-' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("-="));
+                    }
+                    Some('-') => {
+                        self.get_next_char();
+                        return Some(Op("--"));
+                    }
+                    _ => return Some(Op("-")),
+                },
+                '*' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("*="));
+                    }
+                    _ => return Some(Op("*")),
+                },
+                '/' => match self.peek_next_char() {
+                    Some('/') => self.pass_inline_comment(),
+                    Some('*') => {
+                        self.get_next_char();
+                        self.pass_block_comment();
+                    }
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("/="));
+                    }
+                    _ => return Some(Op("/")),
+                },
+                '&' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("&="));
+                    }
+                    Some('-') => {
+                        self.get_next_char();
+                        return Some(Op("&&"));
+                    }
+                    _ => return Some(Op("&")),
+                },
+                '|' => match self.peek_next_char() {
+                    Some('=') => {
+                        self.get_next_char();
+                        return Some(Assignment("|="));
+                    }
+                    Some('-') => {
+                        self.get_next_char();
+                        return Some(Op("||"));
+                    }
+                    _ => return Some(Op("|")),
+                },
+                '>' => match self.peek_next_3() {
+                    (Some('>'), Some('>'), Some('=')) => {
+                        self.get_next_char();
+                        self.get_next_char();
+                        self.get_next_char();
+                        return Some(Assignment(">>>="));
+                    }
+                    (Some('>'), Some('>'), _) => {
+                        self.get_next_char();
+                        self.get_next_char();
+                        return Some(Op(">>>"));
+                    }
+                    (Some('>'), Some('='), _) => {
+                        self.get_next_char();
+                        self.get_next_char();
+                        return Some(Assignment(">="));
+                    }
+                    (Some('>'), _, _) => {
+                        self.get_next_char();
+                        return Some(Op(">>"));
+                    }
+                    (Some('='), _, _) => {
+                        self.get_next_char();
+                        return Some(Op(">="));
+                    }
+                    _ => return Some(GreaterThan),
+                },
+                '<' => match self.peek_next_2() {
+                    (Some('<'), Some('=')) => {
+                        self.get_next_char();
+                        self.get_next_char();
+                        return Some(Assignment("<<="));
+                    }
+                    (Some('<'), _) => {
+                        self.get_next_char();
+                        return Some(Op("<<"));
+                    }
+                    (Some('='), _) => {
+                        self.get_next_char();
+                        return Some(Op("<="));
+                    }
+                    _ => return Some(LessThan),
+                },
+                '\'' => return self.get_char_literal(),
+                '\"' => return self.get_string_literal(),
+                c if c.is_alphanumeric() => {
+                    let s = self.get_identifier_chain();
+                    if matches!(
+                        s,
+                        "abstract"
+                            | "class"
+                            | "const"
+                            | "default"
+                            | "enum"
+                            | "extends"
+                            | "final"
+                            | "implements"
+                            | "import"
+                            | "interface"
+                            | "package"
+                            | "public"
+                            | "private"
+                            | "static"
+                            | "synchronized"
+                            | "transient"
+                            | "return"
+                            | "void"
+                            | "if"
+                            | "else"
+                            | "while"
+                            | "for"
+                            | "new"
+                            | "this"
+                            | "super"
+                            | "break"
+                            | "continue"
+                            | "case"
+                            | "try"
+                            | "catch"
+                            | "do"
+                            | "finally"
+                            | "instanceof"
+                            | "native"
+                            | "protected"
+                            | "switch"
+                            | "throw"
+                            | "throws"
+                            | "volatile"
+                    ) {
+                        return Some(Keyword::<'a>(s));
+                    } else {
+                        return Some(Identifier::<'a>(s));
+                    }
+                }
                 _ => return None,
             }
         }
     }
 
-    fn get_next_char(&mut self) -> Option<&char> {
-        self.ind += 1;
-        self.str.get(self.ind - 1)
+    fn get_identifier_chain(&mut self) -> &'a str {
+        let start_ind = self.ind - self.curr_char.unwrap().len_utf8();
+        let mut iter = self.s[self.ind..].chars();
+        loop {
+            if let Some(c) = iter.next() {
+                if c.is_alphanumeric() || c == '_' || c == '$' {
+                    self.ind += c.len_utf8();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return &self.s[start_ind..self.ind];
     }
 
-    fn peek_next_char(&self) -> Option<&char> {
-        self.str.get(self.ind)
+    fn get_char_literal(&mut self) -> Option<Token<'a>> {
+        let start_index = self.ind - 1;
+        let mut iter = self.s[self.ind..].chars();
+        loop {
+            if let Some(mut c) = iter.next() {
+                self.ind += c.len_utf8();
+                if c == '\\' {
+                    // consume the next char too
+                    c = iter.next()?;
+                    self.ind += c.len_utf8();
+                } else if c == '\'' {
+                    break;
+                }
+            } else {
+                return None;
+            }
+        }
+        Some(Literal(&self.s[start_index..self.ind]))
     }
 
-    fn peek_char_offset(&self, offset: usize) -> Option<&char> {
-        self.str.get(self.ind + offset)
+    fn get_string_literal(&mut self) -> Option<Token<'a>> {
+        let start_index = self.ind - 1;
+        let mut iter = self.s[self.ind..].chars();
+        loop {
+            if let Some(mut c) = iter.next() {
+                self.ind += c.len_utf8();
+                if c == '\\' {
+                    c = iter.next()?;
+                    self.ind += c.len_utf8();
+                } else if c == '\"' {
+                    break;
+                }
+            }
+        }
+        Some(Literal(&self.s[start_index..self.ind]))
+    }
+    fn pass_inline_comment(&mut self) {
+        let mut c = self.get_next_char();
+        while c != None && c != Some('\n') {
+            c = self.get_next_char();
+        }
     }
 
-    fn peek_next_2(&self) -> (Option<&char>, Option<&char>) {
-        (self.peek_char_offset(0), self.peek_char_offset(1))
+    fn pass_block_comment(&mut self) {
+        loop {
+            match (self.get_next_char(), self.peek_next_char()) {
+                (None, _) | (_, None) => return,
+                (Some('*'), Some('/')) => {
+                    self.get_next_char();
+                    return;
+                }
+                _ => {}
+            };
+        }
+    }
+    fn get_next_char(&mut self) -> Option<char> {
+        self.curr_char = self.s[self.ind..].chars().next();
+        self.ind += self.curr_char?.len_utf8();
+        self.curr_char
     }
 
-    fn peek_next_3(&self) -> (Option<&char>, Option<&char>, Option<&char>) {
-        (
-            self.peek_char_offset(0),
-            self.peek_char_offset(1),
-            self.peek_char_offset(2),
-        )
+    fn peek_next_char(&self) -> Option<char> {
+        self.s[self.ind..].chars().next()
+    }
+
+    fn peek_char_offset(&self, offset: usize) -> Option<char> {
+        self.s[self.ind..].chars().nth(offset)
+    }
+
+    fn peek_next_2(&self) -> (Option<char>, Option<char>) {
+        let mut s = self.s[self.ind..].chars();
+        (s.next(), s.next())
+    }
+
+    fn peek_next_3(&self) -> (Option<char>, Option<char>, Option<char>) {
+        let mut s = self.s[self.ind..].chars();
+        (s.next(), s.next(), s.next())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_lexer(lexer: &mut Lexer, token: Token) {
+        assert_eq!(lexer.get_next_token().unwrap(), token);
+    }
+
+    #[test]
+    fn test_str_lit() {
+        let mut lexer = Lexer::new("\"a\\n\\r\\\\a\"");
+        lexer.get_next_char();
+        let s = lexer.get_string_literal();
+        assert!(s.unwrap() == Literal("\"a\\n\\r\\\\a\""));
+    }
+
+    #[test]
+    fn test_char_lit() {
+        let mut lexer = Lexer::new("'\\r'");
+        lexer.get_next_char();
+        let s = lexer.get_char_literal();
+        assert!(s.unwrap() == Literal("'\\r'"))
+    }
+
+    #[test]
+    fn test_long_stuff() {
+        let mut lexer = Lexer::new("String s = \"Hello World!\";");
+        assert!(lexer.get_next_token().unwrap() == Token::Identifier("String"));
+        assert!(lexer.get_next_token().unwrap() == Token::Identifier("s"));
+        assert!(lexer.get_next_token().unwrap() == Token::Assignment("="));
+        assert!(lexer.get_next_token().unwrap() == Token::Literal("\"Hello World!\""));
+        assert!(lexer.get_next_token().unwrap() == Token::Semicolon);
+        assert!(lexer.get_next_token().unwrap() == Token::EOF);
+    }
+
+    #[test]
+    fn test_keyword_and_identifier() {
+        let mut lexer = Lexer::new("public class Integer { private int i; }");
+        let mut lex = |token: Token| test_lexer(&mut lexer, token);
+        lex(Keyword("public"));
+        lex(Keyword("class"));
+        lex(Identifier("Integer"));
+        lex(LBrace);
+        lex(Keyword("private"));
+        lex(Identifier("int"));
+        lex(Identifier("i"));
+        lex(Semicolon);
+        lex(RBrace);
     }
 }
